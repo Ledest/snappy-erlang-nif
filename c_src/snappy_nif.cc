7d3833d8b16c671bcd17ee3e0d61ff4c97498c57
Led <ledest@gmail.com>
2015-02-04 02:27:47 +0200

    shrink and format code

--- c_src/snappy_nif.cc
+++ c_src/snappy_nif.cc
@@ -41,7 +41,7 @@ class SnappyNifSink : public snappy::Sink
     public:
         SnappyNifSink(ErlNifEnv* e);
         ~SnappyNifSink();
-        
+
         void Append(const char* data, size_t n);
         char* GetAppendBuffer(size_t len, char* scratch);
         ErlNifBinary& getBin();
@@ -54,7 +54,7 @@ class SnappyNifSink : public snappy::Sink
 
 SnappyNifSink::SnappyNifSink(ErlNifEnv* e) : env(e), length(0)
 {
-    if(!enif_alloc_binary_compat(env, 0, &bin)) {
+    if (!enif_alloc_binary_compat(env, 0, &bin)) {
         env = NULL;
         throw std::bad_alloc();
     }
@@ -62,96 +62,74 @@ SnappyNifSink::SnappyNifSink(ErlNifEnv* e) : env(e), length(0)
 
 SnappyNifSink::~SnappyNifSink()
 {
-    if(env != NULL) {
+    if (env != NULL)
         enif_release_binary_compat(env, &bin);
-    }
 }
 
-void
-SnappyNifSink::Append(const char *data, size_t n)
+void SnappyNifSink::Append(const char *data, size_t n)
 {
-    if(data != (SC_PTR(bin.data) + length)) {
+    if (data != (SC_PTR(bin.data) + length))
         memcpy(bin.data + length, data, n);
-    }
     length += n;
 }
 
-char*
-SnappyNifSink::GetAppendBuffer(size_t len, char* scratch)
+char* SnappyNifSink::GetAppendBuffer(size_t len, char* scratch)
 {
-    size_t sz;
-    
-    if((length + len) > bin.size) {
-        sz = (len * 4) < 8192 ? 8192 : (len * 4);
+    if (length + len > bin.size) {
+        size_t sz = len * 4;
 
-        if(!enif_realloc_binary_compat(env, &bin, bin.size + sz)) {
+        if (!enif_realloc_binary_compat(env, &bin, bin.size + (sz < 8192 ? 8192 : sz)))
             throw std::bad_alloc();
-        }
     }
 
     return SC_PTR(bin.data) + length;
 }
 
-ErlNifBinary&
-SnappyNifSink::getBin()
+ErlNifBinary& SnappyNifSink::getBin()
 {
-    if(bin.size > length) {
-        if(!enif_realloc_binary_compat(env, &bin, length)) {
-            throw std::bad_alloc();
-        }
-    }
+    if (bin.size > length && !enif_realloc_binary_compat(env, &bin, length))
+        throw std::bad_alloc();
+
     return bin;
 }
 
-
-static inline ERL_NIF_TERM
-make_atom(ErlNifEnv* env, const char* name)
+static inline ERL_NIF_TERM make_atom(ErlNifEnv* env, const char* name)
 {
     ERL_NIF_TERM ret;
-    if(enif_make_existing_atom_compat(env, name, &ret, ERL_NIF_LATIN1)) {
-        return ret;
-    }
-    return enif_make_atom(env, name);
+
+    return enif_make_existing_atom_compat(env, name, &ret, ERL_NIF_LATIN1)
+           ? ret : enif_make_atom(env, name);
 }
 
-
-static inline ERL_NIF_TERM
-make_ok(ErlNifEnv* env, ERL_NIF_TERM mesg)
+static inline ERL_NIF_TERM make_ok(ErlNifEnv* env, ERL_NIF_TERM mesg)
 {
-    ERL_NIF_TERM ok = make_atom(env, "ok");
-    return enif_make_tuple2(env, ok, mesg);   
+    return enif_make_tuple2(env, make_atom(env, "ok"), mesg);
 }
 
-
-static inline ERL_NIF_TERM
-make_error(ErlNifEnv* env, const char* mesg)
+static inline ERL_NIF_TERM make_error(ErlNifEnv* env, const char* mesg)
 {
-    ERL_NIF_TERM error = make_atom(env, "error");
-    return enif_make_tuple2(env, error, make_atom(env, mesg));
+    return enif_make_tuple2(env, make_atom(env, "error"), make_atom(env, mesg));
 }
-
 
 BEGIN_C
-
 
 ERL_NIF_TERM
 snappy_compress_erl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     ErlNifBinary input;
 
-    if(!enif_inspect_iolist_as_binary(env, argv[0], &input)) {
+    if (!enif_inspect_iolist_as_binary(env, argv[0], &input))
         return enif_make_badarg(env);
-    }
 
     // If empty binary has been provided, return an empty binary.
     // Snappy will do this in any case, so might as well skip the
     // overhead...
-    if(input.size == 0) {
+    if (input.size == 0) {
         ErlNifBinary empty;
         // init empty;
-        memset(&empty,0,sizeof(ErlNifBinary));
-        return make_ok(env, enif_make_binary(env,&empty));
-     }
+        memset(&empty, 0, sizeof(ErlNifBinary));
+        return make_ok(env, enif_make_binary(env, &empty));
+    }
 
     try {
         snappy::ByteArraySource source(SC_PTR(input.data), input.size);
@@ -171,104 +149,81 @@ snappy_decompress_erl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     ErlNifBinary bin;
     ErlNifBinary ret;
-    size_t len;
 
-    if(!enif_inspect_iolist_as_binary(env, argv[0], &bin)) {
+    if (!enif_inspect_iolist_as_binary(env, argv[0], &bin))
         return enif_make_badarg(env);
-    }
 
     // Check that the binary is not empty
-    if(bin.size == 0) {
+    if (bin.size == 0) {
         // Snappy library cannot decompress an empty binary - although
         // it will unfortunately let you compress one. If an empty binary
         // has been passed - send an empty binary back.
-        memset(&ret,0,sizeof(ErlNifBinary));
-        return make_ok(env, enif_make_binary(env,&ret));
+        memset(&ret, 0, sizeof(ErlNifBinary));
+ok:
+        return make_ok(env, enif_make_binary(env, &ret));
     }
 
     try {
-        if(!snappy::GetUncompressedLength(SC_PTR(bin.data), bin.size, &len)) {
+        size_t len;
+
+        if (!snappy::GetUncompressedLength(SC_PTR(bin.data), bin.size, &len))
             return make_error(env, "data_not_compressed");
-        }
-
-        if(!enif_alloc_binary_compat(env, len, &ret)) {
+        else if (!enif_alloc_binary_compat(env, len, &ret))
             return make_error(env, "insufficient_memory");
-        }
-
-        if(!snappy::RawUncompress(SC_PTR(bin.data), bin.size,
-                                            SC_PTR(ret.data))) {
+        else if (!snappy::RawUncompress(SC_PTR(bin.data), bin.size, SC_PTR(ret.data)))
             return make_error(env, "corrupted_data");
-        }
-
-        return make_ok(env, enif_make_binary(env, &ret));
+        else
+            goto ok;
     } catch(...) {
         return make_error(env, "unknown");
     }
 }
 
-
-ERL_NIF_TERM
-snappy_uncompressed_length_erl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+ERL_NIF_TERM snappy_uncompressed_length_erl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     ErlNifBinary bin;
     size_t len;
 
-    if(!enif_inspect_iolist_as_binary(env, argv[0], &bin)) {
+    if(!enif_inspect_iolist_as_binary(env, argv[0], &bin))
         return enif_make_badarg(env);
-    }
 
     try {
-        if(!snappy::GetUncompressedLength(SC_PTR(bin.data), bin.size, &len)) {
-            return make_error(env, "data_not_compressed");
-        }
-        return make_ok(env, enif_make_ulong(env, len));
+        return snappy::GetUncompressedLength(SC_PTR(bin.data), bin.size, &len)
+               ? make_ok(env, enif_make_ulong(env, len))
+               : make_error(env, "data_not_compressed");
     } catch(...) {
         return make_error(env, "unknown");
     }
 }
 
-
-ERL_NIF_TERM
-snappy_is_valid(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+ERL_NIF_TERM snappy_is_valid(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     ErlNifBinary bin;
 
-    if (!enif_inspect_iolist_as_binary(env, argv[0], &bin)) {
+    if (!enif_inspect_iolist_as_binary(env, argv[0], &bin))
         return enif_make_badarg(env);
-    }
 
     try {
-        if(snappy::IsValidCompressedBuffer(SC_PTR(bin.data), bin.size)) {
-            return make_atom(env, "true");
-        } else {
-            return make_atom(env, "false");
-        }
+        return make_atom(env, snappy::IsValidCompressedBuffer(SC_PTR(bin.data), bin.size) ? "true" : "false");
     } catch(...) {
         return make_error(env, "unknown");
     }
 }
 
-
-int
-on_load(ErlNifEnv* env, void** priv, ERL_NIF_TERM info)
+int on_load(ErlNifEnv* env, void** priv, ERL_NIF_TERM info)
 {
     return 0;
 }
 
-
-int
-on_reload(ErlNifEnv* env, void** priv, ERL_NIF_TERM info)
+int on_reload(ErlNifEnv* env, void** priv, ERL_NIF_TERM info)
 {
     return 0;
 }
 
-
-int
-on_upgrade(ErlNifEnv* env, void** priv, void** old_priv, ERL_NIF_TERM info)
+int on_upgrade(ErlNifEnv* env, void** priv, void** old_priv, ERL_NIF_TERM info)
 {
     return 0;
 }
-
 
 static ErlNifFunc nif_functions[] = {
     {"compress", 1, snappy_compress_erl},
@@ -277,8 +232,6 @@ static ErlNifFunc nif_functions[] = {
     {"is_valid", 1, snappy_is_valid}
 };
 
-
 ERL_NIF_INIT(snappy, nif_functions, &on_load, &on_reload, &on_upgrade, NULL);
-
 
 END_C
